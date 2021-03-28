fa3826bdd63b065acdcb580b84349ab9d9d36ee9
Mohana810 <79496726+Mohana810@users.noreply.github.com>
2021-03-28 15:52:38 +0530

    Update FindAstring.py

--- FindAstring.py
+++ FindAstring.py
@@ -1,14 +1,3 @@
-# #from collections import Counter
-#
-# string = 'mohanasrana'#input().strip()
-# substring = 'aan' #input().strip()
-#
-# while substring in string:
-#     print(substring)
-#     string = string[string.find(substring)+1:]
-#
-# # string = string[string.find('ana')+1:]
-# # print(string)
 s = input()
 sub = input()
 cnt = 0
